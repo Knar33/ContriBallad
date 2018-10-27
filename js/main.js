f09d5f2cd916b2b97c61a69a5f8b666398ea2c5d
Knar <knarxr@protonmail.com>
2018-10-27 15:07:40 -0500

    Added comments for GetUserContributions

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const UserContributions = new {};
+
+function GetUserContributions(userID) {
+    //Make API call to https://github.com/users/userID/contributions
+    //Set the inner HTML of the graph to the response
+    //Parse the data into an array of Day/Contributions
+    //set that array to UserContributions.DailyContributions
+}
+
+function PlaySong() {
+    
+}
