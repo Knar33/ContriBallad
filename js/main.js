2159bc1d13f854c64afcb6e791c01719813d15a0
Knar <knarxr@protonmail.com>
2018-10-27 16:40:36 -0500

    Starting to put the logic for getting user contributions - cross-domain requests are a problem though

--- js/main.js
+++ js/main.js
@@ -1,12 +1,45 @@
-const UserContributions = new {};
+const UserContributions = {};
 
 function GetUserContributions(userID) {
     //Make API call to https://github.com/users/userID/contributions
-    //Set the inner HTML of the graph to the response
+    var resData;
+
+    $("graphFrame").url = "https://github.com/users/" + userID + "/contributions";
+
+    $.ajax({
+        url: "https://github.com/users/" + userID + "/contributions",
+        method: 'get',
+        crossDomain: true,
+        dataType: 'jsonp html',
+        statusCode: {
+            200: function (data) {
+                resData = data;
+                $("#graph").InnerHTML = data;
+            },
+            404: function (data) {
+                resData = data;
+                $("#graph").InnerHTML = "That user does not exist.";
+            },
+        }
+    });
+
     //Parse the data into an array of Day/Contributions
+    const calendarGraph = $(".js-calendar-graph")[0];
+    const dailyContributions = $(".day");
+    const contributions = [];
+
+    for (let day of dailyContributions){
+        contributions.push({
+            date: day.getAttribute("data-date"),
+            count: day.getAttribute("data-count")
+        }); 
+    }
+    
     //set that array to UserContributions.DailyContributions
+    UserContributions.DailyContributions = contributions;
 }
+  
 
 function PlaySong() {
-    
+
 }
