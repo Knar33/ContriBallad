b8f260f66f5d4a48cda26b37af1787c812351b3b
Knar <knarxr@protonmail.com>
2018-10-28 19:49:40 -0500

    Added endpoint to request page from github

--- ContriBallad/Controllers/ContributionsController.cs
+++ ContriBallad/Controllers/ContributionsController.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 
 namespace ContriBallad.Controllers
 {
-    public class ContributionsController : Controller
+    [System.Web.Http.RoutePrefix("Contributions")]
+    public class ContributionsController : ApiController
     {
-        // GET: Contributions
-        public ActionResult Index()
+        [System.Web.Http.HttpGet, System.Web.Http.Route("{id}")]
+        public async Task<HttpResponseMessage> Get(string id, CancellationToken token = default(CancellationToken))
         {
-            return View();
+            try
+            {
+                string response = "";
+
+                HttpClient client = new HttpClient();
+                var res = await client.GetAsync("https://github.com/users/{0}/contributions" + id);
+                response = await res.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
         }
     }
 }
