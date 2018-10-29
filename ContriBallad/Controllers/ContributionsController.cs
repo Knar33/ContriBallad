518cb1f8e89fc9864861db12df6a8a240103c447
Knar <knarxr@protonmail.com>
2018-10-28 19:35:32 -0500

    Made the base site work in mvc

---
+++ ContriBallad/Controllers/ContributionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContriBallad.Controllers
+{
+    public class ContributionsController : Controller
+    {
+        // GET: Contributions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
